e1d9c0fa395cdefe3d6f81bfb7f5b3df93f2b314
Steven Thewissen <info@getpixls.com>
2021-09-03 14:27:53 +0200

    Meh

--- lunarphase.js
+++ lunarphase.js
@@ -1,26 +1,3 @@
-const fetch = require('node-fetch');
-const { openseaCollectionsUrl } = require('../config.json');
-const Discord = require('discord.js');
-
-module.exports = {
-	name: process.env.DISCORD_FLOOR_COMMAND || "wenmoon",
-	execute(message, args) {
-
-  // Otherwise, current date will be used:
-  const phase = lunarPhase;
-  const age = lunarAge;
-  const emoji = lunarPhaseEmoji;
-
-  console.log(phase);
-  console.log(age);
-  console.log(emoji);
-  
-  const embedMsg = new Discord.MessageEmbed()
-  .setTitle(`${emoji} Tonight's a ${phase} ${emoji}`);
-
-  message.channel.send(embedMsg);
-}};
-
 const Hemisphere = {
   NORTHERN: "Northern",
   SOUTHERN: "Southern",
