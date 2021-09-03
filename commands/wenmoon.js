c5b921454d85ee54856d3936b6e1113ef0e49a4b
Steven Thewissen <info@getpixls.com>
2021-09-03 14:07:09 +0200

    Wen moon?

--- commands/wenmoon.js
+++ commands/wenmoon.js
@@ -1,50 +1,20 @@
 const fetch = require('node-fetch');
 const { openseaCollectionsUrl } = require('../config.json');
-
+import * as Moon from 'lunarphase-js';
 const Discord = require('discord.js');
 
 module.exports = {
 	name: process.env.DISCORD_FLOOR_COMMAND || "wenmoon",
 	execute(message, args) {
 
-  const start = new Date();
-  console.log(start);
-  moonType = moon_phase(start);  
+  // For a specific date, pass a date object to a function:
+  const date = new Date();
+  const phase = Moon.getLunarPhase(date);
+  // Otherwise, current date will be used:
+  const phase = Moon.getLunarPhase();
 
   const embedMsg = new Discord.MessageEmbed()
-  .setTitle(`Tonight's a ${moonType}`);
+  .setTitle(`Tonight's a ${phase}`);
 
   message.channel.send(embedMsg);
 }};
-
-function moon_phase(date) { // ported from http://www.voidware.com/moon_phase.htm
-  var year = date.getYear(),
-      month = date.getMonth(),
-      day = date.getDay();
-
-  if (month < 3) {
-      year--;
-      month += 12;
-  }
-
-  ++month;
-
-  jd = 365.25 * year + 30.6 * month + day - 694039.09; // jd is total days elapsed
-  jd /= 29.53; // divide by the moon cycle (29.53 days)
-  phase = parseInt(jd, 10); // int(jd) -> phase, take integer part of jd
-  jd -= phase; // subtract integer part to leave fractional part of original jd
-  phase = Math.ceil(jd * 8); // scale fraction from 0-8 and round by adding 0.5
-  phase = phase & 7; // 0 and 8 are the same so turn 8 into 0
-
-  switch (phase) {
-      case 0: phase = "New Moon"; break;
-      case 1: phase = "Waxing Crescent Moon"; break;
-      case 2: phase = "Quarter Moon"; break;
-      case 3: phase = "Waxing Gibbous Moon"; break;
-      case 4: phase = "Full Moon"; break;
-      case 5: phase = "Waning Gibbous Moon"; break;
-      case 6: phase = "Last Quarter Moon";
-      case 7: phase = "Waning Crescent Moon"; break;
-  }
-  return phase;
-}
