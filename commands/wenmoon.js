2790e42c3fecf89ede4d5eb43ca6a925e3b96fb4
Steven Thewissen <info@getpixls.com>
2021-09-03 14:08:19 +0200

    With emoji

--- commands/wenmoon.js
+++ commands/wenmoon.js
@@ -7,14 +7,12 @@ module.exports = {
 	name: process.env.DISCORD_FLOOR_COMMAND || "wenmoon",
 	execute(message, args) {
 
-  // For a specific date, pass a date object to a function:
-  const date = new Date();
-  const phase = Moon.getLunarPhase(date);
   // Otherwise, current date will be used:
   const phase = Moon.getLunarPhase();
-
+  const emoji = Moon.getLunarPhaseEmoji();
+  
   const embedMsg = new Discord.MessageEmbed()
-  .setTitle(`Tonight's a ${phase}`);
+  .setTitle(`${emoji} Tonight's a ${phase} ${emoji}`);
 
   message.channel.send(embedMsg);
 }};
