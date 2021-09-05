1e047eb0c516f6537b2fe57a4129b39324f2c3f8
Steven Thewissen <info@getpixls.com>
2021-09-05 20:22:47 +0200

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -3,5 +3,5 @@ const Discord = require('discord.js');
 module.exports = {
 	name: process.env.DISCORD_COMMANDS_COMMAND || "commands",
 	execute(message, args) {
-      return message.channel.send('```!floor - Get the floor price for Pixls.\n!stats - Get a collection of stats for Pixls.\n!token [id] - Get all attributes for a given Pixl.\n!wenmoon - Find out what the moon is up to.\n!ceiling - Some feel good.```');
+      return message.channel.send('```!floor - Get the floor price for Pixls.\n!stats - Get a collection of stats for Pixls.\n!token [id] - Get all attributes for a given Pixl.\n!wenmoon - Find out what the moon is up to.\n!ceiling - Some feel good.\n!recruitme - Huh, who put this here...```');
 }};
