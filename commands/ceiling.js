93a600a1f86cb70bafbde3be09950883bc13149a
Steven Thewissen <info@getpixls.com>
2021-09-05 09:17:07 +0200

    Added new commands

--- commands/ceiling.js
+++ commands/ceiling.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	name: process.env.DISCORD_MESSAGE_COMMAND || "ceiling",
+	name: process.env.DISCORD_CEILING_COMMAND || "ceiling",
 	execute(message, args) {
       return message.channel.send(`The only ceiling is your imagination!`);
 }};
