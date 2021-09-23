3b867a6b4ba7f3ccbf60ac7c0caa52b897eb6162
Steven Thewissen <info@getpixls.com>
2021-09-23 10:52:09 +0200

    Added song :D

--- commands/recruitme.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = {
-	name: process.env.DISCORD_RECRUITME_COMMAND || "recruitme",
-	execute(message, args) {
-		return message.channel.send(`Too late dum dum. Got enough recruits! ${message.author}`);
-	}
-};
---
+++ commands/song.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: process.env.DISCORD_RECRUITME_COMMAND || "song",
+	execute(message, args) {
+
+		var arr = [
+			"*NSYNC - It's Gonna Be Me", 
+			"My Chemical Romance - Helena", 
+			"Backstreet Boys - The Call", 
+			"Die Antwoord - Baby's On Fire",
+			"Fall Out Boy - Sugar, We're Going Down",
+			"Panic! At The Disco - I Write Sins Not Tragedies",
+			"Paramore - Misery Business",
+			"The Red JUmpsuit Apparatus - Face Down",
+			"My Chemical Romance - I'm Not Okay (I Promise)",
+			"All-American Rejects - Dirty Little Secret",
+			"Paramore - That's What You Get",
+			"Yellowcard - Ocean Avenue",
+			"Yellowcard - Only One",
+			"My Chemical Romance - Welcome To The Black Parade",
+			"30 Seconds To Mars - The Kill"
+		];
+
+		return message.channel.send(`Currently listening to: ${arr[Math.floor(Math.random() * arr.length)]} ${message.author}`);
+	}
+};
