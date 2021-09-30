7ee127c0f1e93ba71ba34611e882625488a39d5a
Steven Thewissen <info@getpixls.com>
2021-09-30 10:56:46 +0200

    More changes

--- commands/lfg.js
+++ commands/lfg.js
@@ -4,5 +4,5 @@ const Discord = require('discord.js');
 module.exports = {
 	name: process.env.DISCORD_COMMANDS_COMMAND || "lfg",
 	execute(message, args) {
-      return message.channel.send('<:pet:892672777918758973> <:pet:892672777918758973> <:pet:892672777918758973>');
+      return message.channel.send('<a:pet:892672777918758973> <a:pet:892672777918758973> <a:pet:892672777918758973>');
 }};
