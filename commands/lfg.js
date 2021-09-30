5331fb48c0b4fea56a02ed7422d31472080b3023
Steven Thewissen <info@getpixls.com>
2021-09-30 11:15:31 +0200

    Update lfg.js

--- commands/lfg.js
+++ commands/lfg.js
@@ -4,6 +4,6 @@ const Discord = require('discord.js');
 module.exports = {
 	name: process.env.DISCORD_COMMANDS_COMMAND || "lfg",
 	execute(message, args, client) {
-		const someEmoji = client.emojis.cache..find(emoji => emoji.name === "pet");
+		const someEmoji = client.emojis.cache.find(emoji => emoji.name === "pet");
 		return message.channel.send(`${someEmoji} ${someEmoji} ${someEmoji}`);
 }};
