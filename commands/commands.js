315961188d5dc75bc3d7625d2d48e0fd9e28b25b
Steven Thewissen <info@getpixls.com>
2022-01-05 14:11:45 +0100

    New car commands

--- commands/commands.js
+++ commands/commands.js
@@ -3,5 +3,5 @@ const Discord = require('discord.js');
 module.exports = {
 	name: process.env.DISCORD_COMMANDS_COMMAND || "commands",
 	execute(message, args) {
-      return message.channel.send('```!stats - Get a collection of stats for Pixls.\n!pixl [id] - Get all attributes for a given Pixl.\n!wenmoon - Find out what the moon is up to.\n!ceiling - Some feel good.\n!lfg - LFG!!!!1!\n!bok - Bok boooook!```');
+      return message.channel.send('```!stats - Get a collection of stats for Pixls.\n!carstats - Get a collection of stats for PCC.\n!pixl [id] - Get all attributes for a given Pixl.\n!car [id] - Get all attributes for a given Car.\n!wenmoon - Find out what the moon is up to.\n!ceiling - Some feel good.\n!lfg - LFG!!!!1!\n!bok - Bok boooook!```');
 }};
