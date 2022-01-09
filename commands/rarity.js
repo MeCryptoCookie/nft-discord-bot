b0591b43627cf798d964379947b590f9922628dc
Steven Thewissen <info@getpixls.com>
2022-01-09 18:05:10 +0100

    Embed fixes

--- commands/rarity.js
+++ commands/rarity.js
@@ -5,6 +5,6 @@ module.exports = {
 	execute(message, args) {
 		message.suppressEmbeds(true);
 		return message.channel.send('For all your rarity needs, we recommend these great tools made by our beloved community member Kris. If you like using them, feel free to donate him a bit of your gas!\n' + 
-		'\n\nRarity @ Pixl Tools: https://pixls.nft-tools.xyz/\nRarity @ Pixlton Car Club Tools: https://pixlton-cars.nft-tools.xyz/.');
+		'\n**Rarity @ Pixl Tools:** <https://pixls.nft-tools.xyz/>\n**Rarity @ Pixlton Car Club Tools:** <https://pixlton-cars.nft-tools.xyz/>.');
 	},
 };
