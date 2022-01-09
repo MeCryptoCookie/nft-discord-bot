258ad3584e79fe32f310fcbceb82b0efd76ff837
Steven Thewissen <info@getpixls.com>
2022-01-09 13:46:28 +0100

    Update howclaim.js

--- commands/howclaim.js
+++ commands/howclaim.js
@@ -4,7 +4,7 @@ const { openseaMetadataUrl } = require('../config.json');
 const Discord = require('discord.js');
 
 module.exports = {
-	name: "howclaim",
+	name: "howclaim" || "claim" || "carclaim",
 	execute(message, args) {
     message.suppressEmbeds(true);
     return message.channel.send('**How can I claim my car?**\nGo to our wallet page (https://getpixls.com/wallet), connect your wallet, select the Pixltonians you want to claim for and hit the claim button.\n\n**How can I check if a Pixltonian has an open claim to a car?**\nGo to https://getpixls.com/wallet and you‘ll be able to enter your Pixltonian‘s ID to check.');
