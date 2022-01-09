27bac2c876605500da2f362c56551de9f1fdc21f
Steven Thewissen <info@getpixls.com>
2022-01-09 18:07:09 +0100

    Update rarity.js

--- commands/rarity.js
+++ commands/rarity.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "rarity",
 	execute(message, args) {
 		message.suppressEmbeds(true);
-		return message.channel.send('For all your rarity needs, we recommend these great tools made by our beloved community member Kris. If you like using them, feel free to donate him a bit of your gas!\n' + 
+		return message.channel.send('For all your rarity needs, we recommend these great tools made by our beloved community member Kris. If you like using them, feel free to donate him a bit of your gas! You can do this through either of the two websites below, they both have a donation section!\n' + 
 		'\n**Rarity @ Pixl Tools:** <https://pixls.nft-tools.xyz/>\n**Rarity @ Pixlton Car Club Tools:** <https://pixlton-cars.nft-tools.xyz/>.');
 	},
 };
