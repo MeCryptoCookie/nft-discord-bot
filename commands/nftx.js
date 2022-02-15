fad0b919c99980f8f27366229b319ccbd70ff83c
Steven Thewissen <info@getpixls.com>
2022-02-15 19:40:59 +0100

    Update nftx.js

--- commands/nftx.js
+++ commands/nftx.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: "nftx",
 	execute(message, args) {
 		message.suppressEmbeds(true);
-		return message.channel.send('Want to learn more about our vault? Go check out our Medium post about it at: ');
+		return message.channel.send('Want to learn more about our vault? Go check out our Medium post about it at: https://medium.com/@pixlton/our-nftx-vault-an-intro-becac2093a1a');
 	},
 };
