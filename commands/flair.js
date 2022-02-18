0ea015ff1f3fa4f26fd4125d673af918d61e21f2
Steven Thewissen <info@getpixls.com>
2022-02-18 12:01:39 +0100

    Update flair.js

--- commands/flair.js
+++ commands/flair.js
@@ -5,6 +5,6 @@ module.exports = {
 	execute(message, args) {
 		message.suppressEmbeds(true);
 		return message.channel.send('So you want to make it big chump!? Here\'s how:\n' + 
-		'\n- **Landlords** is 50+\n- **Slumlords** is 25-49\n- **Smol Chickens** is 10-24\n- **From Out of Town** is 1 - 9');
+		'\n- **Landlords** is 50+\n- **Slumlords** is 25-49\n- **Smol Chickens** is 10-24\n- **From Out of Town** is 2 - 9\n- **Fresh Egg** is 1');
 	},
 };
