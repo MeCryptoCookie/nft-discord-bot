3e7b37e4819b25fae776d764026de956ea378d5d
Steven Thewissen <info@getpixls.com>
2022-03-09 14:16:17 +0100

    Update bro.js

--- commands/bro.js
+++ commands/bro.js
@@ -4,5 +4,10 @@ const Discord = require('discord.js');
 module.exports = {
 	name: "bro",
 	execute(message, args) {
-      return message.channel.send('Bro!');
+	  if (!args.length) {
+      	return message.channel.send('Bro!');
+	  }
+	  else {
+		return message.channel.send(`${args[0]} bro!`);
+	  }
 }};
