ebe0086cce1900fbd75878a98cc722aff04907b7
Steven Thewissen <info@getpixls.com>
2022-03-14 20:00:05 +0100

    Remove msg

--- commands/message.js
+++ commands/message.js
@@ -1,29 +1,29 @@
-const Discord = require('discord.js');
+// const Discord = require('discord.js');
 
-module.exports = {
-	name: process.env.DISCORD_MESSAGE_COMMAND || "message",
-	execute(message, args, client) {
+// module.exports = {
+// 	name: process.env.DISCORD_MESSAGE_COMMAND || "message",
+// 	execute(message, args, client) {
 
-    if(!message.member.hasPermission("BAN_MEMBERS"))
-    {
-      return message.channel.send('Nah bro!');
-    }
+//     if(!message.member.hasPermission("BAN_MEMBERS"))
+//     {
+//       return message.channel.send('Nah bro!');
+//     }
 
-    if (!args.length) {
-      return message.channel.send(`You didn't provide a channel ID or message, ${message.author}!`);
-    }
+//     if (!args.length) {
+//       return message.channel.send(`You didn't provide a channel ID or message, ${message.author}!`);
+//     }
 
-    if (isNaN(parseInt(args[0]))) {
-      return message.channel.send(`Channel id must be a number!`);
-    }
+//     if (isNaN(parseInt(args[0]))) {
+//       return message.channel.send(`Channel id must be a number!`);
+//     }
 
-    if(args[1].startsWith("http")) {
-      let url = args[1];      
-      const channel = client.channels.cache.get(args[0]);
-      channel.send(args.slice(2).join(' '), {files:[url]});
-    }
-    else {
-      const channel = client.channels.cache.get(args[0]);
-      channel.send(args.slice(1).join(' '));
-    }
-}};
+//     if(args[1].startsWith("http")) {
+//       let url = args[1];      
+//       const channel = client.channels.cache.get(args[0]);
+//       channel.send(args.slice(2).join(' '), {files:[url]});
+//     }
+//     else {
+//       const channel = client.channels.cache.get(args[0]);
+//       channel.send(args.slice(1).join(' '));
+//     }
+// }};
