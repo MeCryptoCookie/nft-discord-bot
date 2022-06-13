31591b41a20c2e5404051efb45989696d2bce609
Steven Thewissen <info@getpixls.com>
2022-06-13 10:43:17 +0200

    Updated NFTX command

--- commands/claim.js
+++ commands/claim.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: "claim",
 	execute(message, args) {
     message.suppressEmbeds(true);
-    return message.channel.send('**How can I claim my car?**\nGo to our wallet page (<https://getpixls.com/wallet>), connect your wallet, select the Pixltonians you want to claim for and hit the claim button.\n\n**How can I check if a Pixltonian has an open claim to a car?**\nGo to <https://getpixls.com/wallet> and you‘ll be able to enter your Pixltonian‘s ID to check.');
+    return message.channel.send('**How can I claim my Peep?**\nGo to our claim page (<https://pixltonnft.com/claim-peeps>), connect your wallet, select the Pixltonians you want to claim for and hit the claim button.\n\n**How can I check if a Pixltonian has an open claim to a Peep?**\nGo to <https://pixltonnft.com/claim-peeps> and you‘ll be able to enter your Pixltonian‘s ID to check.');
 	},
 };
--- commands/nftx.js
+++ commands/nftx.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: "nftx",
 	execute(message, args) {
 		message.suppressEmbeds(true);
-		return message.channel.send('Want to learn more about our vault? Go check out our Medium post about it at: https://medium.com/@pixlton/our-nftx-vault-an-intro-becac2093a1a');
+		return message.channel.send('Want to learn more about our vault? Go check out our Medium post about it at: https://mirror.xyz/pixlton.eth/OYgDABIQ5NzJPRfcFEgkOvb5zlg_yAuR5KZh4Y-JdIo');
 	},
 };
